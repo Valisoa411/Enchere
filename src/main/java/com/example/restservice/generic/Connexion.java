9f50fbc569ee703a95e262235e0eab253d67c26c
Valisoa411 <99267101+Valisoa411@users.noreply.github.com>
2023-01-20 18:06:56 +0300

    Update Connexion.java

--- src/main/java/com/example/restservice/generic/Connexion.java
+++ src/main/java/com/example/restservice/generic/Connexion.java
@@ -10,21 +10,7 @@ public class Connexion {
         try{
             System.out.println("getConnexion Try to connect");
             Class.forName("org.postgresql.Driver");
-            connect = DriverManager.getConnection("postgresql://containers-us-west-63.railway.app:7844/railway","postgres","ZxdGIUkUQgff5awb9j6B");
-            System.out.println("getConnexion Connected");
-        }
-        catch(Exception e){
-            
-        }
-        return connect;
-    }
-
-    public static Connection getConnexionMongo(){
-        Connection connect = null;
-        try{
-            System.out.println("getConnexion Try to connect");
-            Class.forName("org.postgresql.Driver");
-            connect = DriverManager.getConnection("postgresql://postgres:ZxdGIUkUQgff5awb9j6B@containers-us-west-63.railway.app:7844/railway");
+            connect = DriverManager.getConnection("jdbc:postgresql://containers-us-west-63.railway.app:7844/railway","postgres","ZxdGIUkUQgff5awb9j6B");
             System.out.println("getConnexion Connected");
         }
         catch(Exception e){
