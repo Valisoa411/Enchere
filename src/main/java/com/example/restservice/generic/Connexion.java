1f50593801818234d74cc015e7a0342406c18c62
Valisoa411 <99267101+Valisoa411@users.noreply.github.com>
2023-01-21 10:36:12 +0300

    Update Connexion.java

--- src/main/java/com/example/restservice/generic/Connexion.java
+++ src/main/java/com/example/restservice/generic/Connexion.java
@@ -10,7 +10,7 @@ public class Connexion {
         try{
             System.out.println("getConnexion Try to connect");
             Class.forName("org.postgresql.Driver");
-            connect = DriverManager.getConnection("jdbc:postgresql://containers-us-west-29.railway.app:7844/railway","postgres","StHk4wo1W32EkhnxNGPe");
+            connect = DriverManager.getConnection("jdbc:postgresql://containers-us-west-29.railway.app:7246/railway","postgres","StHk4wo1W32EkhnxNGPe");
             System.out.println("getConnexion Connected");
         }
         catch(Exception e){
